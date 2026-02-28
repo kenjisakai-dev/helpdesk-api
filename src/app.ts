dc2949a4c40d309c8f53071157f5bcc57248424f
Kenji <kenjisakai80@gmail.com>
2026-02-28 20:24:08 -0300

    feat: add file upload settings and include an example image

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from "express";
 import cors from "cors";
 import { routes } from "@/routes/index";
 import { errorHandling } from "@/middlewares/error-handling";
+import uploadConfig from "@/configs/multer-config";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 app.use(errorHandling);
 
